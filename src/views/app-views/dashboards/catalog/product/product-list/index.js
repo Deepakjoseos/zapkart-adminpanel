b9512fdbb0bba2cbb23f723be8d3698f41c39357
Afras <muhammedafras0@gmail.com>
2022-08-18 16:18:01 +0530

    adding upload

--- src/views/app-views/dashboards/catalog/product/product-list/index.js
+++ src/views/app-views/dashboards/catalog/product/product-list/index.js
@@ -363,7 +363,7 @@ const ProductList = () => {
 
   const onSearch = (e) => {
     const value = e.currentTarget.value
-    const searchArray =   searchBackupList
+    const searchArray = searchBackupList
     const data = utils.wildCardSearch(searchArray, value)
     setList(data)
     setSelectedRowKeys([])
