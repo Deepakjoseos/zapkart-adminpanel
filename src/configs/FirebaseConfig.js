934ed7b02c8b89540935388d7ffe9b944508099a
Afrass <afras.p@riolabz.com>
2022-09-09 22:43:45 +0530

    chsngeas

--- src/configs/FirebaseConfig.js
+++ src/configs/FirebaseConfig.js
@@ -1,14 +1,6 @@
 let FirebaseConfig = {}
 
 // FirebaseConfig = {
-//   // apiKey: 'AIzaSyAOWoRQGoegkmwnCca7B2GG5GUG_ZP1lcM',
-//   // authDomain: 'emilus.firebaseapp.com',
-//   // databaseURL: 'https://emilus.firebaseio.com',
-//   // projectId: 'emilus',
-//   // storageBucket: 'emilus.appspot.com',
-//   // messagingSenderId: '807555350717',
-//   // appId: '1:807555350717:web:145ba7c21af47203a2f7d4',
-//   // measurementId: 'G-8KE7HJB252'
 
 //   apiKey: 'AIzaSyBUdVQKSYiMrWXKZLhpcR4pmtIUFa-40w0',
 //   authDomain: 'zapkartnew2022-dc8a8.firebaseapp.com',
@@ -24,40 +16,52 @@ let FirebaseConfig = {}
 //   // messagingSenderId: '1031062435017',
 //   // appId: '1:1031062435017:web:d6b5ac03cd40d344480918',
 // }
-
-switch (process.env.NODE_ENV) {
-  case 'development':
-    FirebaseConfig = {
-      apiKey: 'AIzaSyBUdVQKSYiMrWXKZLhpcR4pmtIUFa-40w0',
-      authDomain: 'zapkartnew2022-dc8a8.firebaseapp.com',
-      projectId: 'zapkartnew2022-dc8a8',
-      storageBucket: 'zapkartnew2022-dc8a8.appspot.com',
-      messagingSenderId: '812753080859',
-      appId: '1:812753080859:web:63d1a2c52ff2a2a5161ebe',
-    }
-    break
-  case 'production':
-    FirebaseConfig = {
-      apiKey: 'AIzaSyDE96sBpGy5JJKHNwTHP3OuUSChRCIgH_U',
-      authDomain: 'ecommerce-74b71.firebaseapp.com',
-      projectId: 'ecommerce-74b71',
-      storageBucket: 'ecommerce-74b71.appspot.com',
-      messagingSenderId: '1031062435017',
-      appId: '1:1031062435017:web:d6b5ac03cd40d344480918',
-    }
-    break
-  case 'test':
-    FirebaseConfig = {
-      apiKey: 'AIzaSyBUdVQKSYiMrWXKZLhpcR4pmtIUFa-40w0',
-      authDomain: 'zapkartnew2022-dc8a8.firebaseapp.com',
-      projectId: 'zapkartnew2022-dc8a8',
-      storageBucket: 'zapkartnew2022-dc8a8.appspot.com',
-      messagingSenderId: '812753080859',
-      appId: '1:812753080859:web:63d1a2c52ff2a2a5161ebe',
-    }
-    break
-  default:
-    break
+if(process.env.REACT_APP_SITE_NAME === 'athathy') {
+  FirebaseConfig = {
+    apiKey: "AIzaSyDIQXZ_KL265rV0hgSvYH1f5EQo010Lnjo",
+    authDomain: "athathy-13dd6.firebaseapp.com",
+    projectId: "athathy-13dd6",
+    storageBucket: "athathy-13dd6.appspot.com",
+    messagingSenderId: "295619267638",
+    appId: "1:295619267638:web:5bb8523b198a33959bdc97"
+  }
+} else {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      FirebaseConfig = {
+        apiKey: 'AIzaSyBUdVQKSYiMrWXKZLhpcR4pmtIUFa-40w0',
+        authDomain: 'zapkartnew2022-dc8a8.firebaseapp.com',
+        projectId: 'zapkartnew2022-dc8a8',
+        storageBucket: 'zapkartnew2022-dc8a8.appspot.com',
+        messagingSenderId: '812753080859',
+        appId: '1:812753080859:web:63d1a2c52ff2a2a5161ebe',
+      }
+      break
+    case 'production':
+      FirebaseConfig = {
+        apiKey: 'AIzaSyDE96sBpGy5JJKHNwTHP3OuUSChRCIgH_U',
+        authDomain: 'ecommerce-74b71.firebaseapp.com',
+        projectId: 'ecommerce-74b71',
+        storageBucket: 'ecommerce-74b71.appspot.com',
+        messagingSenderId: '1031062435017',
+        appId: '1:1031062435017:web:d6b5ac03cd40d344480918',
+      }
+      break
+    case 'test':
+      FirebaseConfig = {
+        apiKey: 'AIzaSyBUdVQKSYiMrWXKZLhpcR4pmtIUFa-40w0',
+        authDomain: 'zapkartnew2022-dc8a8.firebaseapp.com',
+        projectId: 'zapkartnew2022-dc8a8',
+        storageBucket: 'zapkartnew2022-dc8a8.appspot.com',
+        messagingSenderId: '812753080859',
+        appId: '1:812753080859:web:63d1a2c52ff2a2a5161ebe',
+      }
+      break
+    default:
+      break
+  }
 }
+
+
 
 export default FirebaseConfig
