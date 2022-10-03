dbd07062cfb48872f9880899f7940c86bee858d3
Afras <muhammedafras0@gmail.com>
2022-10-03 21:29:19 +0530

    changes

--- src/views/app-views/dashboards/delivery-location/index.js
+++ src/views/app-views/dashboards/delivery-location/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
 import DeliveryZone from './deliveryzone'
-import DeliveryLocation from './delivery-location'
+// import DeliveryLocation from './delivery-location'
 
 const DeliveryLocations = ({ match }) => {
   return (
@@ -10,12 +10,12 @@ const DeliveryLocations = ({ match }) => {
       <Redirect
         exact
         from={`${match.url}`}
-        to={`${match.url}/delivery-location/delivery-location-list`}
+        to={`${match.url}/deliveryzone/deliveryzone-list`}
       />
-      <Route
+      {/* <Route
         path={`${match.url}/delivery-location`}
         component={DeliveryLocation}
-      />
+      /> */}
       <Route path={`${match.url}/deliveryzone`} component={DeliveryZone} />
     </Switch>
   )
