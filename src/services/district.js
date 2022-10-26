f35e9af6ca6be57ca0afcd23ddf805b08f2ab298
Rohan K <rohithkamal@gmail.com>
2022-10-26 21:37:16 +0530

    Revert "removed changes and passing static value DISTRICT"
    This reverts commit bad016fd208e8f2b8aa37632160c0e54183e516f.

--- src/services/district.js
+++ src/services/district.js
@@ -1,4 +1,6 @@
 import fetch from "auth/FetchInterceptor";
+import countryService from "./country";
+import stateService from "./state";
 
 const districtService = {};
 const api = `/district`;
@@ -46,12 +48,30 @@ districtService.getDistrictById = async function (id) {
 
 districtService.createDistrict = async function (data) {
   try {
+    let state = await stateService.getState();
+    if (state.data.length === 0) {
+      let country = await countryService.getCountry();
+      if (country.data.length === 0)
+        country = await countryService.createCountry({
+          name: "COUNTRY_",
+          priority: 1,
+          status: "Active",
+        });
+
+      state = await stateService.createState({
+        name: "STATE_",
+        countryId: country.data.id || country.data[0].id,
+        priority: 2,
+        status: "Active",
+      });
+    }
+
     const res = await fetch({
       url: `/district`,
       method: "post",
       data: {
         ...data,
-        stateId: "",
+        stateId: state.data.id || state.data[0].id,
       },
     });
     return res;
