948949a8e4eea4112ba0796d9cd71516ac260bf3
Afras <muhammedafras0@gmail.com>
2022-11-12 12:08:59 +0530

    changes

--- src/views/app-views/dashboards/shipments/pickupLocation/PickupLocationForm/GeneralField.js
+++ src/views/app-views/dashboards/shipments/pickupLocation/PickupLocationForm/GeneralField.js
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { Input, Row, Col, Card, Form,Select, InputNumber ,TreeSelect} from 'antd'
-const {Option} = Select
+import {
+  Input,
+  Row,
+  Col,
+  Card,
+  Form,
+  Select,
+  InputNumber,
+  TreeSelect,
+} from 'antd'
+const { Option } = Select
 const SITE_NAME = process.env.REACT_APP_SITE_NAME
 
-const GeneralField = ({ form ,vendors,state,pincode,city,setFieldValue,getPincode,getCity}) => {
-  console.log("hiiii")
+const GeneralField = ({
+  form,
+  vendors,
+  state,
+  pincode,
+  city,
+  setFieldValue,
+  getPincode,
+  getCity,
+}) => {
+  console.log('hiiii')
   console.log(pincode)
   return (
     <Card>
@@ -24,8 +42,6 @@ const GeneralField = ({ form ,vendors,state,pincode,city,setFieldValue,getPincod
           </Form.Item>
         </Col>
 
-
-
         <Col span={12}>
           <Form.Item
             name="email"
@@ -42,7 +58,6 @@ const GeneralField = ({ form ,vendors,state,pincode,city,setFieldValue,getPincod
         </Col>
       </Row>
 
-
       <Row gutter={16}>
         <Col span={12}>
           <Form.Item
@@ -58,78 +73,127 @@ const GeneralField = ({ form ,vendors,state,pincode,city,setFieldValue,getPincod
             <Input placeholder="Phone" type="tel" />
           </Form.Item>
         </Col>
-       
-      
-      
-        <Col span={12}>
 
-
-
-       
-
- <Form.Item name="state" label={SITE_NAME === 'zapkart' ? 'State' : 'Emirates'} >
-      <Select placeholder={SITE_NAME === 'zapkart' ? 'State' : 'Emirates'} 
-      
-      
-     
-                   
-                    onChange={(val) => {
-                      form.setFieldsValue({ city: null })
-                      getCity("",`stateName=${val}`)
-                    }}
-                  >
-                    {state.map((item) => (
-                      <Option key={item.name} value={item.name}>
-                        {item.name}
-                      </Option>
-                    ))}
-                  </Select>
-    </Form.Item>
-    </Col>
-    </Row>
-    
-    <Row gutter={16}>
-        <Col span={12}>
- <Form.Item name="city" label={SITE_NAME === 'zapkart' ? 'City' : 'Emirates'} >
-      <Select placeholder={SITE_NAME === 'zapkart' ? 'City' : 'Emirates'}
-
-     
-       >
-         <Option value=""></Option>
-         {city.map((city) => (
-           <Option key={city.name} value={city.name}>
-             {city.name}
-           </Option>
-         ))}
-                </Select>
-
-    </Form.Item>
-    </Col>
-   
-    
-        <Col span={12}>
-    <Form.Item name="pin_code" label={SITE_NAME === 'zapkart' ? 'Pincode' : 'City'} >
-    <Select
-          //  disabled={mode === 'EDIT' ? true : false}
-            showSearch
-            optionFilterProp="children"
-            filterOption={(input, option) =>
-              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-            placeholder="Pincode"
-        
+        {/* <Col span={12}>
+          <Form.Item
+            name="state"
+            label={SITE_NAME === 'zapkart' ? 'State' : 'Emirates'}
           >
-            {pincode.map((pincode) => (
-              <Option key={pincode.name} value={pincode.name}>
-                {pincode.name}
-              </Option>
-            ))}
-          </Select>
-    </Form.Item>
+            <Select
+              placeholder={SITE_NAME === 'zapkart' ? 'State' : 'Emirates'}
+              onChange={(val) => {
+                form.setFieldsValue({ city: null })
+                getCity('', `stateName=${val}`)
+              }}
+            >
+              {state.map((item) => (
+                <Option key={item.name} value={item.name}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col> */}
+      </Row>
 
+      <Row gutter={16}>
+        <Col span={8}>
+          {SITE_NAME === 'zapkart' && (
+            <Form.Item name="state" label="State">
+              <Select
+                placeholder="State"
+                onChange={(val) => {
+                  form.setFieldsValue({ city: null, zipcode: null })
+                  getCity(`stateName=${val}`)
+                }}
+              >
+                {state.map((item) => (
+                  <Option key={item.name} value={item.name}>
+                    {item.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          )}
         </Col>
-        
-        </Row>
+        <Col span={8}>
+          <Form.Item
+            name="city"
+            label={SITE_NAME === 'zapkart' ? 'City' : 'Emirates'}
+          >
+            <Select
+              placeholder={SITE_NAME === 'zapkart' ? 'City' : 'Emirates'}
+              onChange={(val) => {
+                form.setFieldsValue({ zipcode: null })
+                getPincode(`cityName=${val}`)
+              }}
+            >
+              {city.map((item) => (
+                <Option key={item.name} value={item.name}>
+                  {item.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+
+        <Col span={8}>
+          <Form.Item
+            name="pin_code"
+            label={SITE_NAME === 'zapkart' ? 'Zipcode' : 'City'}
+          >
+            <Select placeholder={SITE_NAME === 'zapkart' ? 'Zipcode' : 'City'}>
+              {pincode.map((pincode) => (
+                <Option key={pincode.name} value={pincode.name}>
+                  {pincode.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+
+      {/* 
+      <Row gutter={16}>
+        <Col span={12}>
+          <Form.Item
+            name="city"
+            label={SITE_NAME === 'zapkart' ? 'City' : 'Emirates'}
+          >
+            <Select placeholder={SITE_NAME === 'zapkart' ? 'City' : 'Emirates'}>
+              <Option value=""></Option>
+              {city.map((city) => (
+                <Option key={city.name} value={city.name}>
+                  {city.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+
+        <Col span={12}>
+          <Form.Item
+            name="pin_code"
+            label={SITE_NAME === 'zapkart' ? 'Pincode' : 'City'}
+          >
+            <Select
+              //  disabled={mode === 'EDIT' ? true : false}
+              showSearch
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+              placeholder="Pincode"
+            >
+              {pincode.map((pincode) => (
+                <Option key={pincode.name} value={pincode.name}>
+                  {pincode.name}
+                </Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row> */}
       <Row gutter={16}>
         <Col span={24}>
           <Form.Item
@@ -156,37 +220,34 @@ const GeneralField = ({ form ,vendors,state,pincode,city,setFieldValue,getPincod
       <Row gutter={16}>
         <Col span={24}>
           <Form.Item name="lat" label="Lattitude">
-            <InputNumber  placeholder="Lattitude" />
+            <InputNumber placeholder="Lattitude" />
           </Form.Item>
         </Col>
       </Row>
       <Row gutter={16}>
         <Col span={24}>
           <Form.Item name="long" label="Longitude">
-            <InputNumber  placeholder="Longitude" />
+            <InputNumber placeholder="Longitude" />
           </Form.Item>
         </Col>
       </Row>
       <Row gutter={16}>
         <Col span={24}>
-        <Form.Item name="vendorId" label="Vendor"  >
-          <Select
-          //  disabled={mode === 'EDIT' ? true : false}
-            showSearch
-            optionFilterProp="children"
-            filterOption={(input, option) =>
-              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-            placeholder="Vendor"
-        
-          >
-            {vendors.map((vendor) => (
-              <Option value={vendor.id}>
-                {vendor.fullName}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
+          <Form.Item name="vendorId" label="Vendor">
+            <Select
+              //  disabled={mode === 'EDIT' ? true : false}
+              showSearch
+              optionFilterProp="children"
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+              placeholder="Vendor"
+            >
+              {vendors.map((vendor) => (
+                <Option value={vendor.id}>{vendor.fullName}</Option>
+              ))}
+            </Select>
+          </Form.Item>
         </Col>
       </Row>
     </Card>
