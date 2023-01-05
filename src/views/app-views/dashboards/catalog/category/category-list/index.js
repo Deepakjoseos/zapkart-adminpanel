11e2bf8e5705fbf96ee41d638bcdded742a47f1d
roithmr <rohitm.raveendran@riolabz.com>
2023-01-05 07:49:28 +0530

    pending filter options in apicalls

--- src/views/app-views/dashboards/catalog/category/category-list/index.js
+++ src/views/app-views/dashboards/catalog/category/category-list/index.js
@@ -299,6 +299,12 @@ const handleTableChange = (newPagination) => {
             <Input placeholder="Search" prefix={<SearchOutlined />} />
           </Form.Item>
         </Col>
+        {/* <Col md={6} sm={24} xs={24} lg={6}> */}
+        <Col md={6} sm={24} xs={24} lg={6}>
+          <Form.Item name="level" label="Level">
+            <Input type='number' placeholder="level(number)" prefix={<SearchOutlined />} />
+          </Form.Item>
+        </Col>
         <Col md={6} sm={24} xs={24} lg={6}>
           <Form.Item name="status" label="Status">
             <Select
@@ -354,6 +360,24 @@ const handleTableChange = (newPagination) => {
               <Option value="">All</Option>
               <Option value="true">Yes</Option>
               <Option value="false">No</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col md={6} sm={24} xs={24} lg={6}>
+          <Form.Item name="orderByName" label="Order By Name">
+            <Select className="w-100" placeholder="Order By Name">
+              <Option value="">All</Option>
+              <Option value="Asc">Asc</Option>
+              <Option value="Desc">Desc</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col md={6} sm={24} xs={24} lg={6}>
+          <Form.Item name="excludePriorityZero" label="Exclude Priority Zero">
+            <Select className="w-100" placeholder="excludePriorityZero">
+              <Option value="">All</Option>
+              <Option value="true">true</Option>
+              <Option value="false">false</Option>
             </Select>
           </Form.Item>
         </Col>
